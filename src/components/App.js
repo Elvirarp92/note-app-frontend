54cee83222288f0306206870de5726bc3172924c
Elvira Ramírez <elvirarp92@gmail.com>
2020-07-03 15:28:20 +0200

    create NoteList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import NotesService from './../services/notes.service'
+
+import NoteList from './noteList/noteList'
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      notes: [],
+    }
+    this.notesService = new NotesService()
+  }
+
+  getAllNotes = () => {
+    this.notesService
+      .getNotes()
+      .then((response) => {
+        this.setState({ notes: response.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount = () => this.getAllNotes()
+
   render() {
     return (
-      <div className='App'>
-        <h1>heyo</h1>
-      </div>
+      <main className='App'>
+        <NoteList />
+      </main>
     )
   }
 }
---
+++ src/components/noteList/noteList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteList = (props) => {
+  return(
+    <section>
+      <h2>Notas</h2>
+    </section>
+  )
+}
+
+export default NoteList
